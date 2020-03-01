dd6ac06f1be403964d8006d973464d0132aec033
Vukknesh <leomcn@hotmail.com>
2020-02-29 22:46:17 -0300

    a

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -119,7 +119,7 @@ else:
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': "postgres",
+            'HOST': os.environ.get('DB_HOST'),
             'PORT': 5432,
         }
     }
