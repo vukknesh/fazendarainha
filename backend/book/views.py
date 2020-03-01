b1168279b39b40ea98ccd5f05b2071ffc75c28cc
Vukknesh <leomcn@hotmail.com>
2020-03-01 11:31:54 -0300

    a

--- backend/book/views.py
+++ backend/book/views.py
@@ -219,8 +219,10 @@ def get_all_resumo(request):
         'data', flat=True
     ).distinct()
     group_by_value = {}
+    print(f'value_list = {value_list}')
 
     for value in value_list:
+        print(f'value = {value}')
         group_by_value[value.month] = ResumoSerializer(
             ResumoMensal.objects.filter(data=value), many=True).data
 
